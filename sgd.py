8659a4843547f9ce9ffac82921817d1bb52fbc79
Zewen HUA <fredzewenhua@gmail.com>
2019-11-25 12:53:21 -0500

    mod

--- sgd.py
+++ sgd.py
@@ -1,10 +1,18 @@
 import numpy as np
+import sympy as sm
 sigma, n, N, t = [.05, .3], [50, 100, 500, 1000], 400, 30
 
-def generateTrainU(sigma, n):
-    negU = [np.random.normal(-0.25, sigma, 4).tolist() for _ in range(n/2)]
-    posU = [np.random.normal(0.25, sigma, 4).tolist() for _ in range(n/2)]
-    return negU + posU
+def generateTrain1(u):
+    trainExample = []
+    return trainExample
+
+def generateTrain2(u):
+    trainExample = []
+    return trainExample
+
+def generateTest(u):
+    testExample = []
+    return testExample
 
 def generateTestU(sigma):
     negU = [np.random.normal(-0.25, sigma, 4).tolist() for _ in range(200)]
@@ -12,5 +20,13 @@ def generateTestU(sigma):
     return negU + posU
 
 def loss(w, x, y):
+    return np.log(1 + np.exp(-y*sum(w[i]*x[i] for i in range(5))))
+
+def sgd(scenario, n):
+    w = [0, 0, 0, 0, 0]
+
+
+
+
 
 
