71b85c47b0cae80be7ee0649ea2af269270c7eda
Zewen HUA <fredzewenhua@gmail.com>
2019-11-25 22:26:42 -0500

    mod

--- sgd.py
+++ sgd.py
@@ -1,19 +1,22 @@
 import numpy as np
 from statistics import mean, stdev
 from math import sqrt
+from random import shuffle
 
 sigmas, ns, N, t, alpha = [.05, .3], [50, 100, 500, 1000], 400, 30, 0.01
 
 def generateU(sigma, n):
-    return [np.random.normal(-0.25, sigma, 4).tolist() for _ in range(n//2)] + \
-           [np.random.normal(0.25, sigma, 4).tolist() for _ in range(n//2)]
+    uset = [[-1] + np.random.normal(-0.25, sigma, 4).tolist() for _ in range(n//2)] + \
+           [[1] + np.random.normal(0.25, sigma, 4).tolist() for _ in range(n//2)]
+    shuffle(uset)
+    return uset
 
 
 def generateSet1(uset):
     trainSet = []
     for each in uset:
-        example = []
-        for k in each:
+        example = [each[0]]
+        for k in each[1:]:
             if -1 <= k <= 1: example.append(k)
             elif k < -1: example.append(-1)
             else: example.append(1)
@@ -24,9 +27,9 @@ def generateSet1(uset):
 def generateSet2(uset):
     trainSet = []
     for each in uset:
-        euclidean = sqrt(sum(each[i] ** 2 for i in range(4)))
+        euclidean = sqrt(sum(each[i] ** 2 for i in range(1, 5)))
         if euclidean <= 1: trainSet.append(each)
-        else: trainSet.append([each[i]/euclidean for i in range(4)])
+        else: trainSet.append([each[0]] + [each[i]/euclidean for i in range(1, 5)])
     return trainSet
 
 
@@ -58,8 +61,8 @@ def euclidean2(temp):
 def sgd(euclidean, trainSet, n):
     w, wset, Ws = [0, 0, 0, 0, 0], [[0, 0, 0, 0, 0]], []
     for i in range(n):
-        z = trainSet[i] + [1]
-        y = -1 if i < n/2 else 1
+        z = trainSet[i][1:] + [1]
+        y = trainSet[i][0]
         param = -y * np.exp(-y * sum(w[i]*z[i] for i in range(5))) \
                 / (1 + np.exp(-y * sum(w[i]*z[i] for i in range(5))))
         G = [param * z[i] for i in range(5)]
@@ -77,8 +80,8 @@ def sgd(euclidean, trainSet, n):
 def test(w, testSet):
     lossSet, errorSet = [], []
     for i, each in enumerate(testSet):
-        x = each + [1]
-        y = -1 if i < 200 else 1
+        x = each[1:]
+        y = each[0]
         lossSet.append(lossfunc(w, x, y))
         errorSet.append(errorfunc(w, x, y))
     loss, error = mean(lossSet), mean(errorSet)
@@ -86,10 +89,10 @@ def test(w, testSet):
 
 
 if __name__ == "__main__":
-    # print("#"*107)
-    # print(" "*47, "|          Logistic loss            |  Classification error")
-    # print("Scenario |  sigma  | n | N |  #trials  |  Mean  |  Std Dev  |  Min  |  Excess Risk  |   Mean   |   Std Dev")
     for sigma in sigmas:
+        print()
+        print("*" * 150)
+        print()
         print("sigma: ", sigma)
         testU = generateU(sigma, N)
         testSet1, testSet2 = generateSet1(testU), generateSet2(testU)
@@ -97,7 +100,6 @@ if __name__ == "__main__":
         for _ in range(30):
             trainU = generateU(sigma, 1000)
             trainSet1, trainSet2 = generateSet1(trainU), generateSet2(trainU)
-            Ws_1_50 = sgd(euclidean1, trainSet1, 50)
             for i in range(4):
                 Ws1 = sgd(euclidean1, trainSet1[:ns[i]], ns[i])
                 Ws2 = sgd(euclidean2, trainSet1[:ns[i]], ns[i])
@@ -131,9 +133,3 @@ if __name__ == "__main__":
             print("Mean: ", meanloss2, "| Std Dev: ", stdloss2, "| Min: ", minloss2, "| Excess Risk: ", risk2)
             print("Classification error")
             print("Mean: ", meanerror2, "| Std Dev: ", stderror2)
-        print()
-        print("*"*150)
-        print()
-
-
-
